9754a37dc296ac2c7d7d570f741ae99d4f88b214
SunHowe <tosunhowe@gmail.com>
2021-08-13 11:54:20 +0800

    implement Pool class

---
+++ Runtime/Pool/Interfaces/IPool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HoweFramework.Pool
+{
+    /// <summary>
+    /// 对象池接口
+    /// </summary>
+    public interface IPool<T> : IDisposable where T : class
+    {
+        /// <summary>
+        /// 返回一个可用实例
+        /// </summary>
+        T Pop();
+
+        /// <summary>
+        /// 将实例存入对象池
+        /// </summary>
+        void Push(T value);
+    }
+}
---
+++ Runtime/Pool/KeyPool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HoweFramework.Pool
+{
+    public class KeyPool<TKey> : IKeyPool<TKey>
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop<T>(TKey key, Func<TKey, T> factory = null) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push<T>(TKey key, T value) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
