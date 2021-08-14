b52bc669f43ac80a534dc36305ef51b42e8421a1
SunHowe <tosunhowe@gmail.com>
2021-08-14 15:17:35 +0800

    HCoreBoostrap

---
+++ Runtime/HCoreBoostrap.cs
@@ -0,0 +1,19 @@
+﻿using CatLib;
+using HoweFramework.Base;
+using HoweFramework.Pool;
+
+namespace HoweFramework
+{
+    /// <summary>
+    /// 引擎核心引导程序
+    /// </summary>
+    public class HCoreBoostrap : IBootstrap
+    {
+        public void Bootstrap()
+        {
+            App.OnResolving<IService>(service => { service.Initialize(); });
+            App.OnRelease<IService>(service => { service.Dispose(); });
+            App.Register(new PoolServiceProvider());
+        }
+    }
+}
---
+++ Runtime/Pool/PoolServiceProvider.cs
@@ -0,0 +1,19 @@
+﻿using CatLib;
+
+namespace HoweFramework.Pool
+{
+    /// <summary>
+    /// 对象池服务提供者
+    /// </summary>
+    public class PoolServiceProvider : IServiceProvider
+    {
+        public void Init()
+        {
+        }
+
+        public void Register()
+        {
+            App.Singleton<IPoolService, PoolServiceImpl>();
+        }
+    }
+}
