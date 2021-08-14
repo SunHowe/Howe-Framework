ba87022d8375fe5f02620d8339b9e65716bacee4
SunHowe <tosunhowe@gmail.com>
2021-08-14 14:48:56 +0800

    id generator utility

---
+++ Runtime/Utilities/IdentifierGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HoweFramework.Pool;
+
+namespace HoweFramework.Utilities
+{
+    /// <summary>
+    /// 唯一id生成器
+    /// </summary>
+    public sealed class IdentifierGenerator : IReset
+    {
+        private int _id;
+
+        public IdentifierGenerator(int idBegin = 1)
+        {
+            _id = idBegin - 1;
+        }
+
+        public int Spawn()
+        {
+            return ++_id;
+        }
+
+        public void Dispose()
+        {
+            _id = 0;
+        }
+
+        public void Reset()
+        {
+            _id = 0;
+        }
+    }
+}
