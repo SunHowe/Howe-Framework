eca06274db1c12177fbac8a5ff8edeeda7ee6979
SunHowe <tosunhowe@gmail.com>
2021-08-18 11:43:39 +0800

    资产服务接口

---
+++ Runtime/Asset/AssetService.cs
@@ -0,0 +1,11 @@
+﻿using CatLib;
+
+namespace HoweFramework.Asset
+{
+    /// <summary>
+    /// 资产服务门面
+    /// </summary>
+    public class AssetService : Facade<IAssetService>
+    {
+    }
+}
---
+++ Runtime/Asset/Interfaces/IAssetService.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HoweFramework.Asset
+{
+    /// <summary>
+    /// 资产服务
+    /// </summary>
+    public interface IAssetService
+    {
+        /// <summary>
+        /// 异步加载资产资源
+        /// </summary>
+        Task<T> LoadAsync<T>(string resKey);
+
+        /// <summary>
+        /// 实例化指定对象
+        /// </summary>
+        Task<GameObject> InstantiateAsync(string resKey);
+
+        /// <summary>
+        /// 异步加载资产资源
+        /// </summary>
+        T LoadSync<T>(string resKey);
+
+        /// <summary>
+        /// 实例化指定对象
+        /// </summary>
+        GameObject InstantiateSync(string resKey);
+
+        /// <summary>
+        /// 释放指定资源
+        /// </summary>
+        void Release<T>(T asset);
+
+        /// <summary>
+        /// 释放指定对象
+        /// </summary>
+        void Release(GameObject gameObject);
+    }
+}
