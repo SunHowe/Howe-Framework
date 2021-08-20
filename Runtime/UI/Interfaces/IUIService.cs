beb39fa08d0d76398195d7a6c410575796494cd7
SunHowe <tosunhowe@gmail.com>
2021-08-20 13:22:01 +0800

    ui service interface

--- Runtime/UI/Interfaces/IUIService.cs
+++ Runtime/UI/Interfaces/IUIService.cs
@@ -7,5 +7,24 @@ namespace HoweFramework.UI.Interfaces
     /// </summary>
     public interface IUIService
     {
+        /// <summary>
+        /// 异步打开UI窗口
+        /// </summary>
+        Task OpenUIAsync(string uiKey, object userData = null);
+
+        /// <summary>
+        /// 同步打开UI窗口
+        /// </summary>
+        void OpenUI(string uiKey, object userData = null);
+
+        /// <summary>
+        /// 关闭UI窗口
+        /// </summary>
+        void CloseUI(string uiKey);
+
+        /// <summary>
+        /// 判断指定UI窗口是否处于显示状态
+        /// </summary>
+        bool HasUI(string uiKey);
     }
 }
